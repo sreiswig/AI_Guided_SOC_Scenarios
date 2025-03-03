7a9cf1c1ca340a8041e4a8a39d9bd181479d91ae
sreiswig <sam.reiswig@gmail.com>
2025-03-02 19:14:45 -0600

    merge

--- obj/Debug/net8.0/AI_Guided_SOC_Scenarios.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI_Guided_SOC_Scenarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Guided_SOC_Scenarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8ece0ec31a4a08226af3ab66a2cc5b05b2961a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22caa78a732859588ceac5cbc51f63000f2c5885")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Guided_SOC_Scenarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Guided_SOC_Scenarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

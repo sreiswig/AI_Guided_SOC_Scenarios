b2aedd1b0b975994e1d863534e5c549abe3d63bd
sreiswig <sam.reiswig@gmail.com>
2025-03-05 01:09:15 -0600

    Learning from scratch

--- Site/obj/Debug/net8.0/Site.AssemblyInfo.cs
+++ Site/obj/Debug/net8.0/Site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88e066c8b91f922b64f650853c182b1e78c462d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc29c2c1f0924f3400b801e93dbe9cf1ddb73f00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

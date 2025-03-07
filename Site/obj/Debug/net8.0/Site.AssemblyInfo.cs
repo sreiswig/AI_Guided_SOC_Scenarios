4bdf9313895ee7a3d9ed027e97ef4562087d31e5
sam <sam.reiswig@gmail.com>
2025-03-07 00:24:24 -0600

    some styling to nav and reverse order chat messages

--- Site/obj/Debug/net8.0/Site.AssemblyInfo.cs
+++ Site/obj/Debug/net8.0/Site.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Site")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3c80586a304f332af8c0536a42741cf3d8bbc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde78bdf4f5a8eb5e45abd1aceeec75c31cb1b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Site")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Site")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
